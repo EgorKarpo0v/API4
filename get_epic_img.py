186e4a5ab44edac5b6b2fa124f5d2491ba10167e
EgorKarpo0v <146757426+EgorKarpo0v@users.noreply.github.com>
2023-12-13 21:34:38 +0500

    Исправил название функции

--- get_epic_img.py
+++ get_epic_img.py
@@ -21,7 +21,7 @@ def get_Earth_image():
         formatted_date = datetime.fromisoformat(epic_date).strftime("%Y/%m/%d")
         link = f"https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{epic_name}.png"
         file_path = os.path.join("images", f"{epic_name}.png")
-        launch_img(link, file_path, params)
+        download_img(link, file_path, params)
         print(epic_name)
         print(formatted_date)
 
