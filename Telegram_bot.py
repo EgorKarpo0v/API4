456d7f25c79f23149571912e2aabf887fe1bfcb7
EgorKarpo0v <146757426+EgorKarpo0v@users.noreply.github.com>
2023-12-13 21:22:52 +0500

    Отформатировал текст

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -4,11 +4,9 @@ import os
 import random
 from time import sleep
 
+
 chat_id = "@SpaceImages11"
 bot = telegram.Bot(token='6571182137:AAG6ZCJRwEF00pUTaXFlu9ay37duginxM5g')
-print(bot.get_me())
-# bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
-# bot.send_document(chat_id=chat_id, document=open('cat.png', "rb"))
 while True:
     folder = "images"
     files = listdir(folder)
