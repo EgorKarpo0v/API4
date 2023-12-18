73fb76d495a54a1e57edba73a8fab657b2e162d0
EgorKarpo0v <146757426+EgorKarpo0v@users.noreply.github.com>
2023-12-18 21:51:27 +0500

    Update Telegram_bot.py

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -4,16 +4,22 @@ import os
 import random
 from time import sleep
 
-time_sleep = 600
-chat_id = "@SpaceImages11"
-token = os.environ["TG_TOKEN"]
-bot = telegram.Bot(token=token)
-while True:
-    folder = "images"
-    files = listdir(folder)
-    random.shuffle(files)
-    for file in files:
-        path = os.path.join(folder, file)
-        with open(path,"rb") as f:
-            bot.send_document(chat_id=chat_id, document = f)
-        sleep(time_sleep)
+
+def main():
+    time_sleep = 600
+    chat_id = "@SpaceImages11"
+    token = os.environ["TG_TOKEN"]
+    bot = telegram.Bot(token=token)
+    while True:
+        folder = "images"
+        files = listdir(folder)
+        random.shuffle(files)
+        for file in files:
+            path = os.path.join(folder, file)
+            with open(path,"rb") as f:
+                bot.send_document(chat_id=chat_id, document = f)
+            sleep(time_sleep)
+
+
+if __name__ == "__main__":
+    main()
